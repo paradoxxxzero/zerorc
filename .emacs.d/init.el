84d70fb0591c900436f232a71cae92716898c850
Florian Mounier <florian.mounier@kozea.fr>
2013-06-18 16:01:41 +0200

    Add some emacs goodies

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,7 +27,11 @@
 (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
 
 ;; Py Regexp
-;;(require 'pyregexp)
+
+;; Github browse file
+(global-set-key (kbd "<XF86HomePage>") 'github-browse-file)
+(global-set-key (kbd "<s-XF86HomePage>") 'github-browse-file-blame)
+
 
 ;; Zencoding Mode
 ;;(require 'zencoding-mode)
@@ -110,10 +114,11 @@
 
 (global-set-key (kbd "<M-dead-circumflex>") 'delete-indentation)
 (global-set-key [C-tab] 'other-window)
-(global-set-key [C-S-tab]
-                (lambda ()
-                  (interactive)
-                  (other-window -1)))
+(global-set-key
+ [C-S-tab]
+ (lambda ()
+   (interactive)
+   (other-window -1)))
 
 (global-set-key [C-up] (lambda (arg)
                          (interactive "p")
@@ -141,14 +146,54 @@
                                  (setq end (point)))
                                (goto-char (+ origin (* (length region) arg) arg))))))
 
-(global-set-key (kbd "C-.") (lambda (arg)
-                              "Kill arg lines backward."
-                              (interactive "p")
-                              (kill-line (- 1 arg))))
+(global-set-key
+ (kbd "C-.")
+ (lambda (arg)
+   "Kill arg lines backward."
+   (interactive "p")
+   (kill-line (- 1 arg))))
 
-;; (global-set-key (kbd "<XF86Calculator>") 'psql-on-region-elearning)
-;; (global-set-key (kbd "<S-XF86Calculator>") 'psql-on-region-hydra)
-;; (global-set-key (kbd "<M-XF86Calculator>") 'psql-on-region-pystil)
+
+(global-set-key
+ (kbd "C-x C-r")
+ (lambda ()
+   "Use `ido-completing-read' to \\[find-file] a recent file"
+   (interactive)
+   (let* ((file-assoc-list
+           (mapcar (lambda (x) (cons (file-name-nondirectory x) x)) recentf-list))
+          (filename-list
+           (remove-duplicates (mapcar #'car file-assoc-list)
+                              :test #'string=))
+          (filename
+           (ido-completing-read "Choose recent file: " filename-list nil t)))
+     (when filename
+       (find-file (cdr (assoc filename file-assoc-list)))))))
+
+
+
+
+(defun shell-command-on-buffer (command)
+  (shell-command-on-region (point-min) (point-max) command nil nil nil t))
+
+(defun pygal-python ()
+  (interactive)
+  (shell-command-on-buffer "~/.envs/pygal/bin/python"))
+
+(defun psql-on-region-elearning (begin end)
+  (interactive "r")
+  (shell-command-on-region begin end "psql -U elearning -d elearning_data" nil nil nil t))
+
+(defun psql-on-region-hydra (begin end)
+  (interactive "r")
+  (shell-command-on-region begin end "psql -U hydra -d hydra" nil nil nil t))
+
+(defun psql-on-region-pystil (begin end)
+  (interactive "r")
+  (shell-command-on-region begin end "psql -U pystil -d pystil" nil nil nil t))
+
+(global-set-key (kbd "<XF86Calculator>") 'psql-on-region-elearning)
+(global-set-key (kbd "<S-XF86Calculator>") 'psql-on-region-hydra)
+(global-set-key (kbd "<M-XF86Calculator>") 'psql-on-region-pystil)
 
 (server-start)
 
@@ -159,6 +204,84 @@
         (concat "ack_and_a_half_custom_type=" (mapconcat 'identity extensions ","))
         "--type" "ack_and_a_half_custom_type"))
 
+;;;; Hippie Expand
+(defun urlget (url)
+  (let ((buff (url-retrieve-synchronously url)))
+    (with-current-buffer buff
+      (end-of-buffer)
+      (move-beginning-of-line nil)
+      (buffer-substring-no-properties (point) (point-max)))))
+
+(defun urlpost (url data)
+  (let ((url-request-method        "POST")
+        (url-request-extra-headers `(("Content-Type" . "application/x-www-form-urlencoded")))
+        (url-request-data          data))
+    (let ((buff (url-retrieve-synchronously url)))
+      (with-current-buffer buff
+        (end-of-buffer)
+        (move-beginning-of-line nil)
+        (buffer-substring-no-properties (point) (point-max))))))
+
+(defun urlpost-google-spelling (term)
+  (let ((url-request-method        "POST")
+        (url-request-extra-headers `(("Content-Type" . "application/x-www-form-urlencoded")))
+        (url-request-data          (concat "<?xml version=\"1.0\" encoding=\"utf-8\" ?><spellrequest textalreadyclipped=\"0\" ignoredups=\"0\" ignoredigits=\"1\" ignoreallcaps=\"1\"><text>" term "</text></spellrequest>")))
+    (let ((buff (url-retrieve-synchronously "http://www.google.com/tbproxy/spell?lang=en")))
+      (with-current-buffer buff
+        (end-of-buffer)
+        (move-beginning-of-line nil)
+        (let ((start (search-forward-regexp "<c.+?>" nil t)))
+          (if (not start)
+              nil
+            (search-forward-regexp "</c>")
+            (split-string (buffer-substring-no-properties start (- (point) 4)) "	")))))))
+
+(defun parseresults (response)
+  (cdr
+   (split-string (replace-regexp-in-string "\\([\]\"\[]\\)" "" response) ",")))
+
+(defun try-expand-google (old)
+  (let ((expansion ()))
+    (if (not old)
+        (progn
+          (he-init-string (he-dabbrev-beg) (point))
+          (setq he-expand-list
+                (if (not (equal he-search-string ""))
+                    (parseresults
+                     (urlget
+                      (concat "http://suggestqueries.google.com/complete/search?client=firefox&hl=fr&q="
+                              he-search-string)))))
+          (setq he-search-loc2 0)))
+    (if (not (equal he-search-string ""))
+        (setq expansion (he-dabbrev-kill-search he-search-string)))
+    (if (not expansion)
+        (progn
+          (if old (he-reset-string))
+          ())
+      (progn
+        (he-substitute-string expansion t)
+        t))))
+
+(defun try-expand-google-spelling (old)
+  (let ((expansion ()))
+    (if (not old)
+        (progn
+          (he-init-string (he-dabbrev-beg) (point))
+          (setq he-expand-list
+                (if (not (equal he-search-string ""))
+                    (urlpost-google-spelling he-search-string)))
+          (setq he-search-loc2 0)))
+    (if (not (equal he-search-string ""))
+        (progn
+          (setq expansion (car he-expand-list))
+          (setq he-expand-list (cdr he-expand-list))))
+    (if (not expansion)
+        (progn
+          (if old (he-reset-string))
+          ())
+      (progn
+        (he-substitute-string expansion t)
+        t))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -169,6 +292,8 @@
  '(backup-by-copying-when-linked t)
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
  '(column-number-mode t)
+ '(css-indent-offset 2)
+ '(hippie-expand-try-functions-list (quote (try-expand-all-abbrevs try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-file-name-partially try-complete-file-name try-expand-list try-expand-line try-expand-google-spelling try-expand-google)))
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
@@ -181,6 +306,7 @@
  '(tab-width 4)
  '(tool-bar-mode nil)
  '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
